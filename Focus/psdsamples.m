cc449d160251604a386fae8237b8c1b81203ed13
David Reens <dave.reens@gmail.com>
2019-05-08 12:09:27 -0600

    Phase Space Samples for Hexapole Chapter.

--- Focus/psdsamples.m
+++ Focus/psdsamples.m
@@ -4,13 +4,14 @@
 
 gc = .7;
 figs = [];
+ms = 1;
 
 %% initial
 figs(end+1) = figure('Position',[0 300 300 300]);
-N = 1e4;
+N = 1e5;
 x = randn(N,1);
 v = randn(N,1);
-plot(x,v,'k.','MarkerSize',1)
+plot(x,v,'k.','MarkerSize',ms)
 L = 8;
 xlim([-L L])
 ylim([-L L])
@@ -19,16 +20,16 @@ ylim([-L L])
 %% free flight
 figs(end+1) = figure('Position',[300 300 300 300]);
 t = 2;
-plot(x,v,'k.','MarkerSize',1,'Color',ones(1,3)*gc)
+plot(x,v,'k.','MarkerSize',ms,'Color',ones(1,3)*gc)
 hold on
-plot(x+v*t,v,'k.','MarkerSize',1)
+plot(x+v*t,v,'k.','MarkerSize',ms)
 xlim([-L L])
 ylim([-L L])
 
 
 %% hexapole action
 figs(end+1) = figure('Position',[0 0 300 300]);
-plot(x+v*t,v,'k.','MarkerSize',1,'Color',ones(1,3)*gc)
+plot(x+v*t,v,'k.','MarkerSize',ms,'Color',ones(1,3)*gc)
 hold on
 
 th = .25*pi;
@@ -41,21 +42,44 @@ a = a - th;
 xxx = r.*cos(a);
 vv = r.*sin(a);
 
-plot(xxx,vv,'k.','MarkerSize',1)
+plot(xxx,vv,'k.','MarkerSize',ms)
 xlim([-L L])
 ylim([-L L])
 
 % show the circle.
-th = linspace(0,2*pi,100);
-hold on
-plot(L*.9*cos(th),L*.9*sin(th),'k-')
+% th = linspace(0,2*pi,100);
+% hold on
+% plot(L*.9*cos(th),L*.9*sin(th),'k-')
 
 %% free flight again
 figs(end+1) = figure('Position',[300 0 300 300]);
-plot(xxx,vv,'k.','MarkerSize',1,'Color',ones(1,3)*gc)
+plot(xxx,vv,'k.','MarkerSize',ms,'Color',ones(1,3)*gc)
 hold on
 
 t=2;
-plot(xxx+vv*t,vv,'k.','MarkerSize',1)
+plot(xxx+vv*t,vv,'k.','MarkerSize',ms)
 xlim([-L L])
 ylim([-L L])
+
+%% velocity compression
+figs(end+1) = figure('Position',[600 300 300 300]);
+plot(x,v,'k.','MarkerSize',ms,'Color',ones(1,3)*gc)
+hold on
+plot(x*2,v/2,'k.','MarkerSize',ms)
+xlim([-L L])
+ylim([-L L])
+
+%% spatial compression
+figs(end+1) = figure('Position',[600 0 300 300]);
+plot(x,v,'k.','MarkerSize',ms,'Color',ones(1,3)*gc)
+hold on
+plot(x/2,v*2,'k.','MarkerSize',ms)
+xlim([-L L])
+ylim([-L L])
+
+%% Print everything out
+for i=1:length(figs)
+    a = get(figs(i),'Children');
+    set(a,'Visible','off');
+    print(figs(i),['PSM' num2str(i)],'-dpng','-r300')
+end
